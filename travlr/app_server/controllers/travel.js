5d6fe5d190ae47066ed2ed7dc3a8a7658f9c2cbb
Sarah <sarah.wagner321@gmail.com>
2025-03-20 20:00:25 -0400

    Module 3 completeled baseline

--- travlr/app_server/controllers/travel.js
+++ travlr/app_server/controllers/travel.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('.data/trips.json','utf8'));
+
 /* GET travel view */
 router.get('/', (req, res) => {
     res.render('travel', { title: 'Travlr Getaways' });
